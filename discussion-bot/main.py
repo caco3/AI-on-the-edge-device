2315da059381707e6e7194c9347c4b8eb56aa759
CaCO3 <caco3@ruinelli.ch>
2024-12-21 22:42:33 +0000

    h

--- discussion-bot/main.py
+++ discussion-bot/main.py
@@ -1,11 +1,21 @@
 import argparse
+import logging
+
+log = logging.getLogger(__name__)
+
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--actor", required=True)
     parser.add_argument("--title", required=True)
     parser.add_argument("--body", required=True)
     args = parser.parse_args()
+
+    log.info(f"Dear @{args.actor}\n\n")
+    log.info(f"title: {args.title}\n\n")
+    log.info(f"body: {args.body}\n")
 
     # write to file
     with open("response.md", "w") as f:
