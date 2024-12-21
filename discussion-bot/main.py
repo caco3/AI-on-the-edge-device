668085dba5f3debaee272888efb72da69d2b0c95
CaCO3 <caco3@ruinelli.ch>
2024-12-21 23:28:42 +0000

    u

--- discussion-bot/main.py
+++ discussion-bot/main.py
@@ -1,4 +1,4 @@
-import argparse
+import os
 import logging
 
 log = logging.getLogger(__name__)
@@ -7,18 +7,16 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--actor", required=True)
-    parser.add_argument("--title", required=True)
-    parser.add_argument("--body", required=True)
-    args = parser.parse_args()
+    actor = os.environ["GH_DISCUSSION_ACTOR"]
+    title = os.environ["GH_DISCUSSION_TITLE"]
+    body = os.environ["GH_DISCUSSION_BODY"].replace("`", "")
 
-    log.info(f"Dear @{args.actor}\n\n")
-    log.info(f"title: {args.title}\n\n")
-    log.info(f"body: {args.body}\n")
+    log.info(f"Dear @{actor}\n\n")
+    log.info(f"title: {title}\n\n")
+    log.info(f"body: {body}\n")
 
     # write to file
     with open("response.md", "w") as f:
-        f.write(f"Dear @{args.actor}\n\n")
-        f.write(f"title: {args.title}\n\n")
-        f.write(f"body: {args.body}\n")
+        f.write(f"Dear @{actor}\n\n")
+        f.write(f"title: {title}\n\n")
+        f.write(f"body: {body}\n")
