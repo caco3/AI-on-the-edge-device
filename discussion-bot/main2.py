343463d0c081ab4b5ab257ca5ae4b4ae0f8dedab
CaCO3 <caco3@ruinelli.ch>
2024-12-21 23:25:43 +0000

    t

--- discussion-bot/main2.py
+++ discussion-bot/main2.py
@@ -2,4 +2,4 @@ import os
 
 print(os.environ["GH_DISCUSSION_ACTOR"])
 print(os.environ["GH_DISCUSSION_TITLE"])
-print(os.environ["GH_DISCUSSION_BODY"])
+print(os.environ["GH_DISCUSSION_BODY"].replace("`", "x"))
