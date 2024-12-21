6c42ed5afd659b15f0fdd36d2092bee3ac3e8000
CaCO3 <caco3@ruinelli.ch>
2024-12-21 23:38:35 +0000

    5

--- discussion-bot/main.py
+++ discussion-bot/main.py
@@ -4,16 +4,74 @@ import logging
 log = logging.getLogger(__name__)
 
 
+# List of trigger words and their responses
+# Not case-sensitive!
+trigger_list = [
+  [["homeassistant", "Home Assistant", "Home-assistant"], "Check HomeAssistant"],
+  [["wrong values"], "Improve ROI, ..."],
+  [["lagging", "late transition", "transition", "early"], "check parameter numberanalogtodigittransitionstart"],
+  [["reflection"], "Improve LED, difusor, ..."],
+  [["LCD", "matrix"], "LCD/Matrix"],
+ # [[""], ""],
+]
+
+
+def process_discussion(actor, title, body, html_file):
+    """Analyses the given title and body and creates a response based on teh found trigegr words"""
+    title = markdown.markdown(title)
+    title = title.replace("p>", "h1>")
+
+    body = body.replace("`", "")  # remove the code starts as it upsets bash
+    body = body.replace("![", "[")  # Replace images with the links to the images
+    body = markdown.markdown(body)
+
+    trigger_word_responses =[]
+
+    for entry in trigger_list:
+        for word in entry[0]:
+            if (word.lower() in title.lower()) or (word.lower() in body.lower()):
+                trigger_word_responses.append(entry[1])
+
+    if len(trigger_word_responses) > 0:
+        response = f"Hi @{actor}"
+        response = """
+
+I am the AIOTED-Bot 🤖
+
+I analyzed your question and would like to help you on your issue.        
+Have you already checked our [documentation](https://jomjol.github.io/AI-on-the-edge-device-docs)?
+
+Here are some useful links:
+
+"""
+
+        for word in trigger_word_responses:
+            response += f" - {word}"
+
+        response += """
+        
+If this all does not help and you need a power user or developer to look into it (after you really studied the documentation), write a reply with the text `help-needed`."""
+
+    else:
+        response = ""
+    
+    return response
+
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
     actor = os.environ["GH_DISCUSSION_ACTOR"]
     title = os.environ["GH_DISCUSSION_TITLE"]
-    body = os.environ["GH_DISCUSSION_BODY"].replace("`", "")
+    body = os.environ["GH_DISCUSSION_BODY"])
 
     log.info(f"Dear @{actor}\n\n")
     log.info(f"title: {title}\n\n")
     log.info(f"body: {body}\n")
+
+    response = process_discussion(actor, title, body)
+
+    log.info(f"response: {response}\n")
 
     # write to file
     with open("response.md", "w") as f:
--- discussion-bot/main2.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print(os.environ["GH_DISCUSSION_ACTOR"])
-print(os.environ["GH_DISCUSSION_TITLE"])
-print(os.environ["GH_DISCUSSION_BODY"].replace("`", "x"))
