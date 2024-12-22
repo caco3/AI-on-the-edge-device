a535762d13669397a4b63e4e3920d66fc2e76cfa
CaCO3 <caco3@ruinelli.ch>
2024-12-22 10:04:27 +0000

    1

--- discussion-bot/bot.py
+++ discussion-bot/bot.py
@@ -1,81 +1,22 @@
 import os
 import logging
-import markdown
+
+import response_generator
 
 log = logging.getLogger(__name__)
 
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-# List of trigger words and their responses
-# [["TRIGGER WORD 1", "TRIGGER WORD 2", ...], "RESPONSE"]
-# The trigger words are NOT case-sensitive!
-trigger_list = [
-  [["homeassistant", "Home Assistant", "Home-assistant"], "Check HomeAssistant"],
-  [["wrong values"], "Improve ROI, ..."],
-  [["lagging", "late transition", "transition", "early"], "Check parameter numberanalogtodigittransitionstart"],
-  [["reflection"], "Improve LED, diffusor, see xxx"],
-  [["LCD", "matrix"], "See LCD/Matrix xxx"],
-  [["Rate too high"], "See the [FAQ](https://jomjol.github.io/AI-on-the-edge-device-docs/FAQs/#rate-too-high-read)"],
- # [[""], ""],
-]
+actor = os.environ["GH_DISCUSSION_ACTOR"]
+title = os.environ["GH_DISCUSSION_TITLE"]
+body = os.environ["GH_DISCUSSION_BODY"]
 
+rg = response_generator.Response_generator()
 
-def process_discussion(actor, title, body):
-    """Analyses the given title and body and creates a response based on teh found trigegr words"""
-    log.info(f"Dear @{actor}")
-    log.info(f"title: {title}")
+response = rg.process_discussion(actor, title, body)
 
-    title = markdown.markdown(title)
-    title = title.replace("p>", "h1>")
+log.info(f"response: {response}")
 
-    body = body.replace("`", "")  # remove the code starts as it upsets bash
-    body = body.replace("![", "[")  # Replace images with the links to the images
-    log.info(f"body: {body}")
-
-    body = markdown.markdown(body)
-
-    trigger_word_responses =[]
-
-    for entry in trigger_list:
-        for word in entry[0]:
-            if (word.lower() in title.lower()) or (word.lower() in body.lower()):
-                trigger_word_responses.append([word, entry[1]])
-
-    if len(trigger_word_responses) > 0:
-        response = f"Hi @{actor}"
-        response += """
-
-I am the AIOTED-Bot 🤖
-
-Have you already checked our [documentation](https://jomjol.github.io/AI-on-the-edge-device-docs)?
-I analyzed your question and would like to help you on your issue.
-Here are some useful links based on your input:
-
-"""
-
-        for finding in trigger_word_responses:
-            response += f" - **{finding[0]}:** {finding[1]}\n"
-
-        response += """
-        
-If this all does not help and you need support of an experienced user or developer to look into it (after you really studied the documentation), write a reply with the text `help-needed`.
-Please be aware that we are a small team and run this project in our private, free time"""
-    else:
-        response = ""
-    
-    return response
-
-
-if __name__ == "__main__":
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-
-    actor = os.environ["GH_DISCUSSION_ACTOR"]
-    title = os.environ["GH_DISCUSSION_TITLE"]
-    body = os.environ["GH_DISCUSSION_BODY"]
-
-    response = process_discussion(actor, title, body)
-
-    log.info(f"response: {response}")
-
-    # write to file
-    with open("response.md", "w") as f:
-        f.write(f"{response}")
+# write to file
+with open("response.md", "w") as f:
+    f.write(f"{response}")
---
+++ discussion-bot/response_generator.py
@@ -0,0 +1,90 @@
+import os
+import logging
+
+log = logging.getLogger(__name__)
+
+class Response_generator():
+    def __init__(self):
+        pass
+
+    # List of trigger words and their responses
+    # [["TRIGGER WORD 1", "TRIGGER WORD 2", ...], "RESPONSE"]
+    # The trigger words are NOT case-sensitive!
+    trigger_list = [
+        [["homeassistant", "Home Assistant", "Home-assistant"], "Check HomeAssistant"],
+        [["wrong values"], "Improve ROI, ..."],
+        [["lagging", "late transition", "transition", "early"], "Check parameter numberanalogtodigittransitionstart"],
+        [["reflection"], "Improve LED, diffusor, see xxx"],
+        [["LCD", "matrix"], "See LCD/Matrix xxx"],
+        [["Rate too high"], "See the [FAQ](https://jomjol.github.io/AI-on-the-edge-device-docs/FAQs/#rate-too-high-read)"],
+        # [[""], ""],
+    ]
+
+    def process_discussion(self, actor, title, body):
+        """Analyses the given title and body and creates a response based on the found trigger words
+        Input and output have to be in the markdown format"""
+        #log.info(f"Dear @{actor}")
+        #log.info(f"title: {title}")
+
+        #title = markdown.markdown(title)
+        #title = title.replace("p>", "h1>")
+
+        #body = body.replace("`", "")  # remove the code starts as it upsets bash
+        #body = body.replace("![", "[")  # Replace images with the links to the images
+        #log.info(f"body: {body}")
+
+        #body = markdown.markdown(body)
+
+        trigger_word_responses = []
+
+        for entry in self.trigger_list:
+            for word in entry[0]:
+                if (word.lower() in title.lower()) or (word.lower() in body.lower()):
+                    trigger_word_responses.append([word, entry[1]])
+
+        if len(trigger_word_responses) > 0:
+            response = f"Hi @{actor}"
+            response += """
+    
+I am the AIOTED-Bot 🤖
+
+Have you already checked our [documentation](https://jomjol.github.io/AI-on-the-edge-device-docs)?
+I analyzed your question and would like to help you on your issue.
+Here are some useful links based on your input:
+
+"""
+
+            for finding in trigger_word_responses:
+                response += f" - **{finding[0]}:** {finding[1]}\n"
+
+            response += """
+    
+If this all does not help and you need support of an experienced user or developer to look into it (after you really studied the documentation), write a reply with the text `help-needed`.
+Please be aware that we are a small team and run this project in our private, free time"""
+        else:
+            response = ""
+
+        return response
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--actor", required=True)
+    parser.add_argument("--title", required=True)
+    parser.add_argument("--body", required=True)
+    args = parser.parse_args()
+
+    actor = args.actor
+    title = args.title
+    body = args.body
+
+    rg = Response_generator()
+
+    response = rg.process_discussion(actor, title, body)
+
+    log.info("response:")
+    log.info(f"{response}")
